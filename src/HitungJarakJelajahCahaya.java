9acfeefcf1b9f7cb284d1adc4f3d0247721490da
hp <sahalnurdin@gmail.com>
2022-10-29 08:01:50 +0700

    add changes

--- src/HitungJarakJelajahCahaya.java
+++ src/HitungJarakJelajahCahaya.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
+import javafx.scene.paint.Stop;
+
 public class HitungJarakJelajahCahaya {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Deklarasi Variabel
+        long jumlahHari, panjangKM, kecepatanCahaya, detik;
+
+        // Input user
+        Scanner systemInput = new Scanner(System.in);
+        System.out.println("Masukkan kecepatan cahaya yang dinginkan ");
+
+        // Menghitung jarak jelajah cahaya dalam detik
+        kecepatanCahaya = systemInput.nextLong();
+        jumlahHari = systemInput.nextLong();
+        detik = jumlahHari * 60 * 60 * 24;
+        panjangKM = kecepatanCahaya * detik;
+
+        System.out.println("Dalam " + jumlahHari + " hari, cahaya menjelajah berkisar " + panjangKM + " km.");
+        systemInput.close();
     }
 }
