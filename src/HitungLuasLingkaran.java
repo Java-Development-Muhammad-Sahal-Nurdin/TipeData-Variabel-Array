6789767f45f2702e6ac81d5ebceeb2ba75e41ce3
hp <sahalnurdin@gmail.com>
2022-10-29 07:47:10 +0700

    add changes

--- src/HitungLuasLingkaran.java
+++ src/HitungLuasLingkaran.java
@@ -1,5 +1,23 @@
-public class HitungLuasLingkaran {
+
+// Problem : Tulisah program yang menggunakan variabel double untuk menghitung luas sebuah lingkaran.
+/// Menghitung Luas lingkaran
+import java.util.Scanner;
+
+class HitungLuasLingkaran {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Deklarasi variabel
+        double pi, r, luasLingkaran;
+
+        pi = 3.14;
+
+        // Menggunakan input user
+        Scanner systemInput = new Scanner(System.in);
+
+        System.out.println("Masukkan panjang dari jari-jari: ");
+        r = systemInput.nextDouble();
+        luasLingkaran = pi * r * r;
+
+        System.out.println("Maka luas lingkaran adalah: " + luasLingkaran);
+
     }
 }
