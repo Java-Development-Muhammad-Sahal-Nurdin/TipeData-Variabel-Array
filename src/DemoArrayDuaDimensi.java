cb89fc320a552dadfd524a507c9c7a3e28b78ade
hp <sahalnurdin@gmail.com>
2022-10-30 11:46:01 +0700

    add files

--- src/DemoArrayDuaDimensi.java
+++ src/DemoArrayDuaDimensi.java
@@ -1,7 +1,21 @@
 // Probelem Tulislah sebuah program yang menggunakan sebuah array dua-dimensi
 public class DemoArrayDuaDimensi {
     public static void main(String[] args) {
+        int duaD[][] = new int[4][5];
+        int i, j, k = 0;
+
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < 5; j++) {
+                duaD[i][j] = k;
+                k++;
+            }
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++)
+                System.out.print(duaD[i][j] + " ");
+            System.out.println();
+
+        }
 
     }
-
 }
--- src/DemoArraySatuDimensi.java
+++ src/DemoArraySatuDimensi.java
@@ -1,7 +1,13 @@
 // Problem : Tulislah sebuah program yang menggunakan sebuah array satu-dimensi 
 public class DemoArraySatuDimensi {
     public static void main(String[] args) {
+        double arrayAngka[] = { 10.1, 11.2, 12.3, 13.4, 14.5 };
+        double hasil = 0;
+        int i;
+
+        for (i = 0; i < 5; i++)
+            hasil = hasil + arrayAngka[i];
+        System.out.println("Rerata adalah " + hasil / 5);
 
     }
-
 }
