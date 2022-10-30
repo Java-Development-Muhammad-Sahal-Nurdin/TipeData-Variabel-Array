1d4437a4668163bc397d28668da5375ef49771ac
hp <sahalnurdin@gmail.com>
2022-10-30 10:54:29 +0700

    add files

--- src/DemoCast.java
+++ src/DemoCast.java
@@ -1,6 +1,21 @@
 // Problem : Tulislah sebuah program yang mendemosntrasikan pelbagai konversi tipe 
 public class DemoCast {
     public static void main(String[] args) {
+        byte b;
+        int i = 257;
+        double d = 323.142;
+
+        System.out.println("\nKonversi dari int menjadi byte.");
+        b = (byte) i;
+        System.out.println(" i dan b " + i + " " + b);
+
+        System.out.println("\nKonversi dari double menjadi int.");
+        i = (int) d;
+        System.out.println(" i dan d " + i + " " + d);
+
+        System.out.println("\nKonversi dari double menjadi byte.");
+        b = (byte) d;
+        System.out.println(" b dan d " + i + " " + b);
 
     }
 
